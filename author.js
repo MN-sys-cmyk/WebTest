644664e9599109a5b27b991b70401b42df4982dc
MN-sys-cmyk <matej.nemrava@gmail.com>
2025-09-08 18:40:37 +0200

    Update author.js

--- author.js
+++ author.js
@@ -1,29 +1,23 @@
 // JavaScript pro stránku s detailem autora
 
-// Funkce pro načtení detailu autora a jeho příspěvků
 function loadAuthorDetails() {
-    // Získáme ID autora z URL parametru
     const urlParams = new URLSearchParams(window.location.search);
     const authorId = urlParams.get('id');
     
-    // Pokud nemáme ID autora, přesměrujeme na hlavní stránku
     if (!authorId) {
         console.error('Nebylo zadáno ID autora');
         window.location.href = 'index.html';
         return;
     }
     
-    // Najdeme autora podle ID
     const author = authorsData.find(a => a.id === authorId);
     
-    // Pokud autor neexistuje, přesměrujeme na hlavní stránku
     if (!author) {
         console.error('Autor s ID ' + authorId + ' nebyl nalezen');
         window.location.href = 'index.html';
         return;
     }
     
-    // Aktualizujeme obsah stránky podle dat autora
     document.title = author.name + ' - LiterárníKomunita';
     
     const authorNameElement = document.getElementById('authorName');
@@ -36,35 +30,27 @@ function loadAuthorDetails() {
     if (authorImageElement) authorImageElement.src = author.image;
     if (authorBioElement) authorBioElement.textContent = author.bio;
     
-    // Načteme příspěvky autora
     loadAuthorPosts(author.name);
 }
 
-// Funkce pro načtení příspěvků autora
 function loadAuthorPosts(authorName) {
-    // Získáme kontejner pro příspěvky
     const postsGrid = document.querySelector('.posts-grid');
     if (!postsGrid) {
         console.error('Kontejner pro příspěvky nebyl nalezen');
         return;
     }
     
-    // Filtrujeme příspěvky autora
     const authorPosts = postsData.filter(post => post.author === authorName);
     
-    // Pokud nejsou žádné příspěvky, zobrazíme zprávu
     if (authorPosts.length === 0) {
         postsGrid.innerHTML = '<p>Tento autor zatím nemá žádné příspěvky.</p>';
         return;
     }
     
-    // Seřadíme příspěvky podle data (nejnovější první)
     authorPosts.sort((a, b) => new Date(b.date) - new Date(a.date));
     
-    // Omezíme počet příspěvků na prvních 6 nejnovějších
     const recentPosts = authorPosts.slice(0, 6);
     
-    // Generujeme HTML pro každý příspěvek
     let postsHTML = '';
     
     recentPosts.forEach(post => {
@@ -84,23 +70,20 @@ function loadAuthorPosts(authorName) {
                             <span>Slovo autora</span>
                             <span class="arrow">▼</span>
                         </div>
-                        <div style="display: none;"><p class="authorWordText">${post.excerpt}</p></div>
+                        <div style="display: none;"><p id="authorWord">${post.excerpt}</p></div>
                     </div>
                 </div>
             </div>
         `;
     });
     
-    // Vložíme HTML do kontejneru
     postsGrid.innerHTML = postsHTML;
     
-    // Upravíme styl mřížky pro správné zobrazení
     postsGrid.style.display = 'flex';
     postsGrid.style.flexWrap = 'wrap';
     postsGrid.style.gap = '30px';
     postsGrid.style.justifyContent = 'flex-start';
 
-    // Inicializace tlačítek slovo autora
     initAuthorWordToggle();
 }
 
